3e4e34e1efe15dffb68a5db11f6cd64c99bdca90
chan <1911270452@qq.com>
2022-07-20 20:13:56 +0800

    feat:inplement practice06,add Klass and some refactor

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Student {
+import java.text.MessageFormat;
+
+public class Student extends Person {
+    private Klass klass;
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        return  generateStudentIntroduceContext();
+    }
+
+    private String generateStudentIntroduceContext() {
+        return  MessageFormat.format("My name is {0}. I am {1} years old. I am a Student. I am at Class 2.",
+                super.getName(), super.getAge());
+    }
+
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,55 @@
 package practice06;
 
-public class Teacher {
+import com.sun.javafx.binding.StringFormatter;
+import javafx.beans.binding.StringExpression;
+
+import java.text.MessageFormat;
+
+public class Teacher extends Person {
+
+    private Klass klass;
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (klass == null) {
+            return generateTeacherIntroduceContextWithNoKlass();
+        }
+        return generateTeacherIntroduceContextWithKlass();
+    }
+
+    private String generateTeacherIntroduceContextWithNoKlass() {
+        return MessageFormat.format("My name is {0}. I am {1} years old. I am a Teacher. I teach No Class.",
+                super.getName(), super.getAge());
+    }
+
+    private String generateTeacherIntroduceContextWithKlass() {
+        return MessageFormat.format("My name is {0}. I am {1} years old. I am a Teacher. I teach Class {2}.",
+                super.getName(), super.getAge(), klass.getNumber());
+    }
+
+    public String introduceWith(Student student) {
+        return student.getKlass().getNumber() == klass.getNumber()
+                ? generateIntroduceWithStudentContext(student)
+                : generateIntroduceWithoutStudentContext(student);
+    }
+
+    private String generateIntroduceWithStudentContext(Student student) {
+        return super.introduce()
+                + MessageFormat.format(" I am a Teacher. I teach {0}.", student.getName());
+    }
+
+    private String generateIntroduceWithoutStudentContext(Student student) {
+        return super.introduce()
+                + " I am a Teacher. I don't teach "+student.getName() + ".";
+    }
 }
