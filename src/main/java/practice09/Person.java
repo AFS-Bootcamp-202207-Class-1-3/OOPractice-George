1b0755468442ef27d8a4c5c83daba96aaf2533de
chan <1911270452@qq.com>
2022-07-20 23:59:15 +0800

    feat:implement practice09,klass convert to classes

--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,32 @@
 package practice09;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+        return id == person.id && name.equals(person.name) && age == person.age;
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,72 @@
 package practice09;
 
-public class Teacher {
+import java.text.MessageFormat;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+
+    private LinkedList<Klass> classes;
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+
+    public String introduce() {
+        if (classes == null || classes.size() == 0) {
+            return generateTeacherIntroduceContextWithNoKlass();
+        }
+        return generateTeacherIntroduceContextWithKlass();
+    }
+
+    private String generateTeacherIntroduceContextWithNoKlass() {
+        return MessageFormat.format("My name is {0}. I am {1} years old. I am a Teacher. I teach No Class.",
+                super.getName(), super.getAge());
+    }
+
+    private String generateTeacherIntroduceContextWithKlass() {
+        return MessageFormat.format("My name is {0}. I am {1} years old. I am a Teacher. I teach Class {2}.",
+                super.getName(),
+                super.getAge(), convertClassesToString(classes));
+    }
+
+    private String convertClassesToString(LinkedList<Klass> classes) {
+        return classes.stream()
+                .map(Klass::getNumber)
+                .collect(Collectors.toList())
+                .toString()
+                .substring(1, classes.size() * 3 - 1);
+    }
+
+
+    public String introduceWith(Student student) {
+        for (Klass klass : classes) {
+            if (klass.getNumber() == student.getKlass().getNumber()) {
+                return generateIntroduceWithStudentContext(student);
+            }
+        }
+        return generateIntroduceWithoutStudentContext(student);
+    }
+
+    private String generateIntroduceWithStudentContext(Student student) {
+        return super.introduce()
+                + MessageFormat.format(" I am a Teacher. I teach {0}.", student.getName());
+    }
+
+    private String generateIntroduceWithoutStudentContext(Student student) {
+        return super.introduce()
+                + " I am a Teacher. I don't teach "+student.getName() + ".";
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return this.classes;
+    }
+
+    public boolean isTeaching(Student student) {
+        return classes.contains(student.getKlass());
+    }
 }
