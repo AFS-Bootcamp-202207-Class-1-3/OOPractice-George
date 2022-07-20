59939d4741da4e943762e0a94bd59d07507c1a78
CHENGE8 <1911270452@qq.com>
2022-07-20 17:04:08 +0800

    feat:implement practice,enhance student class and worker class introduce

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,24 @@
 package practice04;
 
-public class Student {
+import java.text.MessageFormat;
+
+public class Student extends Person {
+    private int klass;
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        return  MessageFormat.format("My name is {0}. I am {1} years old. I am a Student. I am at Class 2.",
+                super.getName(), super.getAge());
+    }
+
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,13 @@
 package practice04;
 
-public class Worker {
+import java.text.MessageFormat;
+
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+    public String introduce() {
+        return MessageFormat.format("My name is {0}. I am {1} years old. I am a Worker. I have a job.",
+                super.getName(), super.getAge());
+    }
 }
