ab8cb7d2c53c0caa30186077e7a355603a196808
CHENGE8 <1911270452@qq.com>
2022-07-20 17:15:43 +0800

    feat:implement practice05,add teacher class

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,28 @@
 package practice05;
 
-public class Teacher {
+import java.text.MessageFormat;
+
+public class Teacher extends Person{
+
+    private int klass;
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (klass == 0) {
+            return MessageFormat.format("My name is {0}. I am {1} years old. I am a Teacher. I teach No Class.",
+                    super.getName(), super.getAge());
+        }
+        return MessageFormat.format("My name is {0}. I am {1} years old. I am a Teacher. I teach Class {2}.",
+                super.getName(), super.getAge(), klass);
+    }
 }
