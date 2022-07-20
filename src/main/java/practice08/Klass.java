830457cf4567efcd8e67c8b28e528a620af3e1e2
chan <1911270452@qq.com>
2022-07-20 21:34:05 +0800

    feat:implement practice08, add appendMember function and enhance some function

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,35 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class 2";
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass().getNumber() != getNumber()) {
+            System.out.print("It is not one of us.\n");
+        } else {
+            this.leader = student;
+            student.setIsLeader(true);
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(new Klass(number));
+    }
 }
